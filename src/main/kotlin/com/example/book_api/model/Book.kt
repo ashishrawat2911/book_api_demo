d74f07a5f832d2453aff369864880d71723e494c
Ashish Rawat <ashishrawat2911@gmail.com>
2020-09-25 13:46:14 +0530

    mapping books with topic

--- src/main/kotlin/com/example/book_api/model/Book.kt
+++ src/main/kotlin/com/example/book_api/model/Book.kt
@@ -5,21 +5,7 @@ import javax.persistence.Id
 import javax.persistence.ManyToOne
 
 @Entity
-class Book {
-    @Id
-    var id: String
-    var name: String
-    var description: String
-
+class Book(@Id var id: String, var name: String, var description: String, topicId: String) {
     @ManyToOne
-    var topic: Topic
-
-    constructor(id: String, name: String, description: String, topicId: String) {
-        this.id = id
-        this.name = name
-        this.description = description
-        this.topic = Topic(topicId, "", "")
-    }
-
-
+    var topic: Topic = Topic(topicId, "", "")
 }
--- src/main/kotlin/com/example/book_api/model/Topic.kt
+++ src/main/kotlin/com/example/book_api/model/Topic.kt
@@ -3,15 +3,4 @@ package com.example.book_api.model
 import javax.persistence.Id
 
 
-class Topic {
-    @Id
-    lateinit var id: String
-    lateinit var name: String
-    lateinit var description: String
-
-    constructor(id: String, name: String, description: String) {
-        this.id = id
-        this.name = name
-        this.description = description
-    }
-}
+data class Topic(@Id var id: String, var name: String, var description: String)
