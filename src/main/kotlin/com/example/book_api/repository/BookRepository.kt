ef16f2b4bac47b62eb88ae5b746ba5c1ce3c7d25
Ashish Rawat <ashishrawat2911@gmail.com>
2020-09-29 15:34:09 +0530

    updated application.properties

--- src/main/kotlin/com/example/book_api/repository/BookRepository.kt
+++ src/main/kotlin/com/example/book_api/repository/BookRepository.kt
@@ -1,11 +1,11 @@
 package com.example.book_api.repository
 
 import com.example.book_api.model.Book
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BookRepository : JpaRepository<Book, String> {
+interface BookRepository : CrudRepository<Book, String> {
 
     fun findByTopicId(topicId: String): List<Book>
 
--- src/main/kotlin/com/example/book_api/repository/TopicRepository.kt
+++ src/main/kotlin/com/example/book_api/repository/TopicRepository.kt
@@ -1,8 +1,8 @@
 package com.example.book_api.repository
 
 import com.example.book_api.model.Topic
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TopicRepository : JpaRepository<Topic, String>
+interface TopicRepository : CrudRepository<Topic, String>
